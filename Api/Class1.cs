5de14a6ef4b5630cbbd1e0012e48ac5f804f2aaf
remi2001 <ometzremi54@gmail.com>
2023-03-02 09:42:16 +0100

    v1

--- Api/Class1.cs
+++ Api/Class1.cs
@@ -1,7 +1,25 @@
-﻿namespace Api
+﻿using System.Net.Http;
+using System.Text.Json.Serialization;
+
+namespace Api
 {
     public class Class1
     {
+        //https://api-lprgi.natono.biz/api/GetConfig
+
+        public class Obj
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("age")]
+            public int Age { get; set; }
+            [JsonIgnore]
+            public DateTime DateNaissance { get; set; }
+        }
+
+
+        string jsonString = "{\"name\": \"toto\", \"age\": 19}";
+        Obj DeserializedObject = JsonConvert.DeserializeObject<Obj>(jsonString);
 
     }
 }
