651f1a26e94ab5d2b6aa4778d78a14cf5bd4bf62
remi2001 <ometzremi54@gmail.com>
2023-03-02 17:24:36 +0100

    Debut Recup API

--- Api/Class1.cs
+++ Api/Class1.cs
@@ -7,19 +7,23 @@ namespace Api
     {
         //https://api-lprgi.natono.biz/api/GetConfig
 
-        public class Obj
+        /*
+        using (var v_HttpClient = new HttpClient())
         {
-            [JsonProperty("name")]
-            public string Name { get; set; }
-            [JsonProperty("age")]
-            public int Age { get; set; }
-            [JsonIgnore]
-            public DateTime DateNaissance { get; set; }
+         HttpResponseMessage response;
+             try
+             {
+                response = await
+                v_HttpClient.GetAsync("http://api-lprgi.natono.biz/api/GetConfig");
+                response.EnsureSuccessStatusCode();
+                string contentResponse = await response.Content.ReadAsStringAsync();
+            }
+             catch(HttpRequestException)
+             {
+                // Handle error
+            }
         }
 
-
-        string jsonString = "{\"name\": \"toto\", \"age\": 19}";
-        Obj DeserializedObject = JsonConvert.DeserializeObject<Obj>(jsonString);
-
+        */
     }
 }
